9622ce04b19272822ab639ae1e9e9f6ebb4c40f5
Arnab Das <abc@xyz.com>
2023-09-09 09:58:18 +0530

    Adder More Examples

--- AdderSubtractorSynchronizedMethod/Subtractor.java
+++ AdderSubtractorSynchronizedMethod/Subtractor.java
@@ -15,7 +15,7 @@ public class Subtractor implements Callable<Void>{
             System.out.println("Subtractor Asking for Lock for " + i);
             //lock.lock();
             
-            val.intValue(i);
+            val.intValue(-1 * i);
             System.out.println("Subtractor Subtracted " + i);
             
             
